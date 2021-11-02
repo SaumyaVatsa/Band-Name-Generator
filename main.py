5e8b7400844e1c23a676b3b8c5cae22d8c68c6b9
Saumya Vatsa <87228860+SaumyaVatsa@users.noreply.github.com>
2021-11-02 10:16:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Ask user in which city he/she grew up in
+city = input("Which city you grew up in?\n")
+
+# Ask user what is his/her pet's name
+
+pet = input("What is your pet's name?\n")
+
+# Print Band Name 
+
+Print("Your Band Name is" + pet + " " + city)
